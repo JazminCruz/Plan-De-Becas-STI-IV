2b3ae6e3d310ab02891b497958d7a94c146dde3e
JazminCruz <jazmin.ceg@gmail.com>
2018-10-09 14:02:08 -0700

    Primer Programa

---
+++ Program-c/Semana1/archivo1-20181009.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int main(){
+int Numero;
+Numero = 1;
+
+
+
+
+printf("hola Jazmin\n");
+printf("hola Jazmin\t");
+printf("\nMi numero es %d",Numero);
+Numero=3;
+printf("\nMi numero es %d",Numero);
+
+
+}
