2d84bb33369e4de41ac57236b0f737d3d0b5f43b
JazminCruz <jazmin.ceg@gmail.com>
2018-10-12 00:22:11 -0700

    Tercer programa

---
+++ Program-c/Semana1/jueves1.c
@@ -0,0 +1,42 @@
+#include<stdio.h>
+
+int main() {
+char letraabc;
+
+
+printf("Introducir una letra:");
+scanf(" %c", &letraabc);
+
+
+
+if ( letraabc == 'a' || letraabc == 'A'){
+    printf("Su letra es una vocal\n");
+   }
+    
+else 
+    if ( letraabc == 'e' || letraabc == 'E'){
+        printf ( "Su letra es una vocal\n");
+    }
+
+else 
+    if ( letraabc == 'i' || letraabc == 'I'){
+     printf("Su letra es una vocal\n");
+    } 
+
+else
+    if (letraabc == 'o' || letraabc == 'O'){
+      printf("Su letra es una vocal\n");
+    }
+else
+    if (letraabc == "u" || letraabc == 'U'){
+      printf("Su letra es un volcal\n");
+    }
+
+else
+   printf("Su letra %c es una CONSONANTE \n", letraabc);
+
+
+
+
+
+}
